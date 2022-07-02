3142258b5351a90dc0f857743e93d8f9fbce4aac
Luis Moncada <luis.moncada.mso@gmail.com>
2022-07-02 22:21:19 +0200

    Initial Commit

---
+++ Test App/Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_AppApp.swift
+//  Test App
+//
+//  Created by Luis Moncada on 02.07.22.
+//
+
+import SwiftUI
+
+@main
+struct Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
